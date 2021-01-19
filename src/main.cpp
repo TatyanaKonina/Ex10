5963a9a4bffcc58d02dc46004246aeb6ae3007f5
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2021-01-19 12:05:00 +0300

    fix headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 // Copyright 2020 Konina Tatiana
-#include <iostream>
-#include <stdlib.h>
 #include "MyStack.h"
 #include "postfix.h"
+#include <stdlib.h>
+#include <iostream>
 
-int main()
-{
+int main() {
   std::string s1("2 + 2");
   std::string s2 = infix2prefix(s1);
   std::cout << s2;  // 2 6 3 * 4 2 - / +
-  std::cout << "hello";
   return 0;
 }
 
