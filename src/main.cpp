9ec93d9b4315b2ddb00377dd167682a96dee6cc1
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2021-01-19 11:54:18 +0300

    add src

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-// Copyright 2020 A.SHT
+// Copyright 2020 Konina Tatiana
 #include <iostream>
-#include <string>
-
+#include <stdlib.h>
+#include "MyStack.h"
 #include "postfix.h"
 
-int main() {
+int main()
+{
+  std::string s1("2 + 2");
+  std::string s2 = infix2prefix(s1);
+  std::cout << s2;  // 2 6 3 * 4 2 - / +
+  std::cout << "hello";
   return 0;
 }
+
+
+
--- src/postfix.cpp
+++ src/postfix.cpp
@@ -1,7 +1,55 @@
-// Copyright 2020 A.SHT
 #include "MyStack.h"
 #include "postfix.h"
+#include <map>
+#include <string>
 
-std::string infix2postfix(std::string infix) {
-  return infix;
+std::string infix2prefix(std::string infix) {
+  std::map<char, int> priority = { {'(', 0}, {')', 1}, {'+', 2},
+                                 {'-', 2}, {'*', 3}, {'/', 3} };
+  std::string all_operations = "+-*/";
+  MyStack<char> operations(200);
+  std::string out;
+  for (int i = 0; i < infix.size(); ++i) {
+    if (infix[i] == ' ')
+      continue;
+    if (all_operations.find(infix[i]) != std::string::npos) {
+      if (operations.isEmpty() || operations.get() == '(' ) {
+        operations.push(infix[i]);
+      }
+      else if (priority[infix[i]] > priority[operations.get()]) {
+        operations.push(infix[i]);
+      }
+      else {
+        while (!(operations.isEmpty())  && ( priority[operations.get()] >= priority[infix[i]]  )) {
+          if (operations.get() == ')') break;
+          out += operations.get();
+          out += ' ';
+          operations.pop();
+        }
+        operations.push(infix[i]);
+      }
+    }
+    else if (infix[i] == '(') {
+      operations.push(infix[i]);
+    }
+    else if (infix[i] == ')') {
+      while (operations.get() != '(') {
+        out += operations.get();
+        out += ' ';
+        operations.pop();
+      }
+      operations.pop();
+    }
+    else {
+      out += infix[i];
+      out += ' ';
+    }
+  }
+  while (!operations.isEmpty()) {
+    out += operations.get();
+    out += ' ';
+    operations.pop();
+  }
+  out.resize(out.size() - 1);
+  return out;
 }
